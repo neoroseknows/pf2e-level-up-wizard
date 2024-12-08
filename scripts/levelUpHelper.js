09ec0ec42f10d6bad3163f9aaf25d9a9dd9721d8
BenABaron <benabaron@gmail.com>
2024-12-08 13:19:07 -0600

    add ability score increase note, spell note, and data info

--- scripts/levelUpHelper.js
+++ scripts/levelUpHelper.js
@@ -1,4 +1,11 @@
 let cachedFeats = null;
+const abilityScoreIncreaseLevels = [5, 10, 15, 20];
+const newSpellRankLevels = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+/**
+ *
+ * @returns feats compendium
+ */
 
 const getCachedFeats = async () => {
   if (!cachedFeats) {
@@ -8,7 +15,20 @@ const getCachedFeats = async () => {
   return cachedFeats;
 };
 
+/**
+ *
+ * @param {string} str string to normalize
+ * @returns string with spaces replaced with dashes and set to lowercase
+ */
+
 const normalizeString = (str) => str.replace(/\s+/g, '-').toLowerCase();
+
+/**
+ *
+ * @param {string} searchQuery feat type to search for
+ * @param {number} toCharacterLevel level that the character is leveling to
+ * @returns feats filtered to the character's level and below, sorted by level (descending) then alphabetically
+ */
 
 const filterAndSortFeats = async (searchQuery, toCharacterLevel) => {
   const feats = await getCachedFeats();
@@ -29,8 +49,14 @@ const filterAndSortFeats = async (searchQuery, toCharacterLevel) => {
   );
 };
 
+/**
+ *
+ * @param {CharacterSheetPF2e} characterData the data for the character sheet to get features for
+ * @param {string} type feature type
+ * @returns filtered/sorted feats based off of the feature type
+ */
+
 const getFeaturesForLevel = async (characterData, type) => {
-  console.log(characterData?.object?.class?.system);
   const levelsArray =
     characterData?.object?.class?.system?.[`${type}FeatLevels`]?.value;
   const toCharacterLevel =
@@ -62,13 +88,43 @@ const getFeaturesForLevel = async (characterData, type) => {
   return filterAndSortFeats(searchQuery, toCharacterLevel);
 };
 
+/**
+ *
+ * @param {CharacterSheetPF2e} characterData data for the character sheet to get features for
+ * @returns boons object for the level that the character is leveling to
+ */
+
 const getBoonsForLevel = (characterData) => {
   const toCharacterLevel =
     characterData?.object?.system?.details?.level?.value + 1;
+  const doesCharacterHaveSpellcasting =
+    characterData?.object?.class?.system?.spellcasting;
   const boonsArray = Object.values(characterData?.object?.class?.system?.items);
 
-  return boonsArray.filter((boon) => boon.level === toCharacterLevel);
+  const boonsForLevel = boonsArray.filter(
+    (boon) => boon.level === toCharacterLevel
+  );
+
+  const abilityScoreIncreaseLevel =
+    abilityScoreIncreaseLevels.includes(toCharacterLevel);
+
+  const newSpellRankLevel =
+    newSpellRankLevels.includes(toCharacterLevel) &&
+    doesCharacterHaveSpellcasting;
+
+  return {
+    boonsForLevel,
+    abilityScoreIncreaseLevel,
+    newSpellRankLevel,
+    doesCharacterHaveSpellcasting
+  };
 };
+
+/**
+ *
+ * @param {CharacterSheetPF2e} characterData data for the character sheet to get features for
+ * @returns array of skills for the user to select from
+ */
 
 const getSkillsForLevel = (characterData) => {
   const toCharacterLevel =
@@ -78,10 +134,14 @@ const getSkillsForLevel = (characterData) => {
 
   if (!levelsArray.includes(toCharacterLevel)) return;
 
-  console.log(characterData?.object?.skills);
-
   return Object.values(characterData?.object?.skills);
 };
+
+/**
+ *
+ * @param {CharacterSheetPF2e} characterData data for the character sheet to get features for
+ * @returns feats for the upcoming level associated with the passed in search query
+ */
 
 const getClassFeaturesForLevel = (characterData) =>
   getFeaturesForLevel(characterData, 'class');
@@ -94,12 +154,6 @@ const getSkillFeaturesForLevel = (characterData) =>
 
 const getGeneralFeaturesForLevel = (characterData) =>
   getFeaturesForLevel(characterData, 'general');
-
-/**
- * TODO:
- * Attribute Boosts
- * Spellcasting
- */
 
 export class PF2eLevelUpHelperConfig extends FormApplication {
   constructor(sheetData) {
@@ -118,13 +172,29 @@ export class PF2eLevelUpHelperConfig extends FormApplication {
   }
 
   async getData() {
+    const classFeats = await getClassFeaturesForLevel(this.sheetData);
+    const ancestryFeats = await getAncestryFeaturesForLevel(this.sheetData);
+    const skillFeats = await getSkillFeaturesForLevel(this.sheetData);
+    const generalFeats = await getGeneralFeaturesForLevel(this.sheetData);
+    const boons = getBoonsForLevel(this.sheetData);
+    const skills = getSkillsForLevel(this.sheetData);
+
+    // Check if at least one field in `boons` is truthy
+    const hasBoonsToDisplay = !!(
+      boons.boonsForLevel.length > 0 ||
+      boons.abilityScoreIncreaseLevel ||
+      boons.newSpellRankLevel ||
+      boons.doesCharacterHaveSpellcasting
+    );
+
     return {
-      classFeats: await getClassFeaturesForLevel(this.sheetData),
-      ancestryFeats: await getAncestryFeaturesForLevel(this.sheetData),
-      skillFeats: await getSkillFeaturesForLevel(this.sheetData),
-      generalFeats: await getGeneralFeaturesForLevel(this.sheetData),
-      boons: getBoonsForLevel(this.sheetData),
-      skills: getSkillsForLevel(this.sheetData)
+      classFeats,
+      ancestryFeats,
+      skillFeats,
+      generalFeats,
+      boons,
+      skills,
+      hasBoonsToDisplay
     };
   }
 }
